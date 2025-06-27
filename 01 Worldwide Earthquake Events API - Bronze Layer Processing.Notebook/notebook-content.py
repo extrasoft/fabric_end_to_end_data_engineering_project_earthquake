b277a2c6128cea9ecaa4697050e564a9ce9feef7
extrasoft <30718043+extrasoft@users.noreply.github.com>
2025-06-27 13:15:34 +0700

    Create the Silver layer notebook

--- 01 Worldwide Earthquake Events API - Bronze Layer Processing.Notebook/notebook-content.py
+++ 01 Worldwide Earthquake Events API - Bronze Layer Processing.Notebook/notebook-content.py
@@ -8,8 +8,14 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse_name": "",
-# META       "default_lakehouse_workspace_id": ""
+# META       "default_lakehouse": "5ad5b400-2657-4122-8f04-7495fade5720",
+# META       "default_lakehouse_name": "earthquakes_lakehouse",
+# META       "default_lakehouse_workspace_id": "45397e97-41aa-433a-96dd-aed0944b7878",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "5ad5b400-2657-4122-8f04-7495fade5720"
+# META         }
+# META       ]
 # META     }
 # META   }
 # META }
@@ -17,6 +23,33 @@
 # MARKDOWN ********************
 
 # # <mark></mark>Worldwide Earthquake Events API - Bronze Layer Processing
+
+# MARKDOWN ********************
+
+# #### Initial default parameters
+
+# CELL ********************
+
+from datetime import date, timedelta
+
+# start_date = previous 7 days
+# end_date = yesterday
+start_date = date.today() - timedelta(7) 
+end_date = date.today() - timedelta(1) 
+
+# url = f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_date}&endtime={end_date}"
+# print(url)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Get response from Earthquake API then load into lakehouse
 
 # CELL ********************
 
@@ -37,8 +70,11 @@ if response.status_code == 200:
     
     # Specify the file name (and path if needed)
     file_path = f'/lakehouse/default/Files/{start_date}_earthquake_data.json'
-    
+
     # Open the file in write mode ('w') and save the JSON data
+    # เปิดไฟล์ใน Lakehouse ตาม file_path 
+    # 'w' = ถ้าไฟล์มีอยู่แล้ว "เขียนทับ" ถ้ายังไม่มี "สร้างไหม่"abs
+    # indent=4 คือ ตอนเขียนไฟล์ .json ลง lakehouse ให้เนื้อหามีแถบเยื้อง = 4 เพื่อให้อ่านง่ายสวยงาม
     with open(file_path, 'w') as file:
         # The `json.dump` method serializes `data` as a JSON formatted stream to `file`
         # `indent=4` makes the file human-readable by adding whitespace
@@ -47,6 +83,19 @@ if response.status_code == 200:
     print(f"Data successfully saved to {file_path}")
 else:
     print("Failed to fetch data. Status code:", response.status_code)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.read.option("multiline", "true").json(f"Files/{start_date}_earthquake_data.json")
+# df now is a Spark DataFrame containing JSON data from "Files/{start_date}_earthquake_data.json".
+display(df)
 
 # METADATA ********************
 
--- 02 Worldwide Earthquake Events API - Silver Layer Processing.Notebook/notebook-content.py
+++ 02 Worldwide Earthquake Events API - Silver Layer Processing.Notebook/notebook-content.py
@@ -8,8 +8,14 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse_name": "",
-# META       "default_lakehouse_workspace_id": ""
+# META       "default_lakehouse": "5ad5b400-2657-4122-8f04-7495fade5720",
+# META       "default_lakehouse_name": "earthquakes_lakehouse",
+# META       "default_lakehouse_workspace_id": "45397e97-41aa-433a-96dd-aed0944b7878",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "5ad5b400-2657-4122-8f04-7495fade5720"
+# META         }
+# META       ]
 # META     }
 # META   }
 # META }
@@ -23,14 +29,54 @@
 from pyspark.sql.functions import col
 from pyspark.sql.types import TimestampType
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Initial default parameters
+
 # CELL ********************
 
-# df now is a Spark DataFrame containing JSON data
+from datetime import date, timedelta
+
+# start_date = previous 7 days
+start_date = date.today() - timedelta(7) 
+print(start_date)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### df now is a Spark DataFrame containing JSON data
+
+# CELL ********************
+
 df = spark.read.option("multiline", "true").json(f"Files/{start_date}_earthquake_data.json")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Reshape earthquake data by extracting and renaming key attributes for further analysis.
 
 # CELL ********************
 
-# Reshape earthquake data by extracting and renaming key attributes for further analysis.
 df = \
 df.\
     select(
@@ -47,16 +93,78 @@ df.\
         col('properties.updated').alias('updated')
         )
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
 # CELL ********************
 
-# Convert 'time' and 'updated' columns from milliseconds to timestamp format for clearer datetime representation.
+display(df.head(10))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Convert 'time' and 'updated' columns from milliseconds to timestamp format for clearer datetime representation.
+
+# CELL ********************
+
 df = df.\
     withColumn('time', col('time')/1000).\
     withColumn('updated', col('updated')/1000).\
     withColumn('time', col('time').cast(TimestampType())).\
     withColumn('updated', col('updated').cast(TimestampType()))
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+display(df.head(10))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# #### Save the Dataframe to Manage Table
+
 # CELL ********************
 
 # appending the data to the gold table
 df.write.mode('append').saveAsTable('earthquake_events_silver')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM earthquakes_lakehouse.earthquake_events_silver LIMIT 1000")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
